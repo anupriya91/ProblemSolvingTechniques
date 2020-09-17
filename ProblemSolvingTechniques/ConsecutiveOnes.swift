455ea076471c6dd160c31a767b58bec84783ee20
Admin <admin@Admins-MacBook-Air.local>
2020-09-17 22:00:11 +0530

    COnsecutive Ones

---
+++ ProblemSolvingTechniques/ConsecutiveOnes.swift
@@ -0,0 +1,29 @@
+//
+//  ConsecutiveOnes.swift
+//  ProblemSolvingTechniques
+//
+//  Created by Admin on 17/09/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import Foundation
+class ConsecutiveOnes {
+    func findMaxConsecutiveOnes(_ nums: [Int]) -> Int {
+        var current : Int = 0
+        var maxConsecutiveOne : Int = 0
+        for i in nums {
+            if i == 1 {
+                current += 1
+            } else {
+                if current > maxConsecutiveOne {
+                    maxConsecutiveOne = current
+                }
+                if maxConsecutiveOne > nums.count/2 {
+                    return maxConsecutiveOne
+                }
+                current = 0
+            }
+        }
+        return maxConsecutiveOne > current ? maxConsecutiveOne : current
+    }
+}
